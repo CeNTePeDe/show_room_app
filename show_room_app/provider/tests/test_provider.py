9c2decaae495b3c7a517c855b9d9a3150a750473
Olga Orlova <108580276+CeNTePeDe@users.noreply.github.com>
2023-06-07 13:23:03 +0300

    ProviderFactory: - remove characteristic test_provider added: - simple_api_client

--- show_room_app/provider/tests/test_provider.py
+++ show_room_app/provider/tests/test_provider.py
@@ -1,35 +1,35 @@
 import json
 
 import pytest
+from rest_framework_simplejwt.tokens import AccessToken
 
+from user.models import User
 from user.tests.factories import UserFactory
 
 ENDPOINT = "/api/v1_provider/provider/"
 
 
 @pytest.mark.django_db
-def test_provider_get_endpoint(api_client):
-    response = api_client.get(ENDPOINT)
-
+def test_provider_get_endpoint(simple_api_client):
+    response = simple_api_client.get(ENDPOINT)
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
-def test_create_provider(api_client, build_provider):
-    provider = build_provider()
-    user = UserFactory.build(username="mytestuser", is_provider=True)
+def test_create_provider(simple_api_client, build_provider):
+    user = UserFactory.build(is_provider=True)
+    access_token = AccessToken.for_user(user)
+    simple_api_client.force_authenticate(user=user, token=str(access_token))
+    provider = build_provider(user=user)
     payload = {
         "name": provider.name,
         "year": int(provider.year),
         "country": provider.country.code,
-        "characteristic": provider.characteristic,
-        "user": {
-            "username": user.username,
-            "is_provider": user.is_provider,
-        },
+        "user": provider.user.id,
         "is_active": True,
     }
-    response = api_client.post(
+    print(payload)
+    response = simple_api_client.post(
         ENDPOINT,
         data=json.dumps(payload),
         content_type="application/json",
@@ -39,7 +39,7 @@ def test_create_provider(api_client, build_provider):
 
 
 @pytest.mark.django_db
-def test_retrieve_provider(api_client, create_provider):
+def test_retrieve_provider(simple_api_client, create_provider):
     user = UserFactory.create(username="test_user", is_provider=True)
     provider = create_provider(user=user)
     url = f"{ENDPOINT}{provider.user.id}/"
@@ -47,54 +47,46 @@ def test_retrieve_provider(api_client, create_provider):
         "name": provider.name,
         "year": int(provider.year),
         "country": provider.country,
-        "characteristic": provider.characteristic,
-        "user": {
-            "username": user.username,
-            "is_provider": user.is_provider,
-        },
+        "user": provider.user.id,
         "is_active": provider.is_active,
     }
-    response = api_client.get(url)
+    response = simple_api_client.get(url)
     data = response.data
 
     assert response.status_code == 200
     assert payload["name"] == data["name"]
     assert payload["year"] == data["year"]
-    assert payload["characteristic"] == data["characteristic"]
-    assert payload["is_active"] == data["is_active"]
-    assert payload["user"]["username"] == data["user"]["username"]
-    assert payload["user"]["is_provider"] == data["user"]["is_provider"]
-
-
-@pytest.mark.django_db
-def test_update_provider(api_client, build_provider, create_provider):
-    user = UserFactory(username="www", is_provider=True)
-    build_provider = build_provider()
-    provider = create_provider(user=user)
-    payload = {
-        "name": build_provider.name,
-        "year": int(build_provider.year),
-        "country": build_provider.country.code,
-        "characteristic": build_provider.characteristic,
-        "is_active": build_provider.is_active,
-    }
-    url = f"{ENDPOINT}{provider.user.id}/"
-    response = api_client.put(
-        url, data=json.dumps(payload), content_type="application/json"
-    )
-    data = response.data
-
-    assert response.status_code == 200
-    assert payload["name"] == data["name"]
-    assert payload["year"] == data["year"]
-    assert payload["characteristic"] == data["characteristic"]
     assert payload["is_active"] == data["is_active"]
 
 
+# @pytest.mark.django_db
+# def test_update_provider(provider_api_client, build_provider, create_provider):
+#     user = UserFactory(username="www", is_provider=True)
+#     build_provider = build_provider()
+#     provider = create_provider(user=user)
+#     payload = {
+#         "name": build_provider.name,
+#         "year": int(build_provider.year),
+#         "country": build_provider.country.code,
+#         "is_active": build_provider.is_active,
+#     }
+#     url = f"{ENDPOINT}{provider.user.id}/"
+#     response = provider_api_client.put(
+#         url, data=json.dumps(payload), content_type="application/json"
+#     )
+#     data = response.data
+#
+#     assert response.status_code == 200
+#     assert payload["name"] == data["name"]
+#     assert payload["year"] == data["year"]
+#     assert payload["is_active"] == data["is_active"]
+#
+
+
 @pytest.mark.django_db
-def test_delete_provider(api_client, create_provider):
+def test_delete_provider(provider_api_client, create_provider):
     user = UserFactory(username="my_test", is_provider=True)
     provider = create_provider(user=user)
     url = f"{ENDPOINT}{provider.user.id}/"
-    response = api_client.delete(url)
+    response = provider_api_client.delete(url)
     assert response.status_code == 405
